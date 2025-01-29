cf757648a020b562dd187e4d1067f786a6c98841
Alexander <oBelyash@yandex.ru>
2025-01-29 22:39:09 +0300

    fix

--- Sweet Boom/View/Shop/CharacterShopView.swift
+++ Sweet Boom/View/Shop/CharacterShopView.swift
@@ -10,7 +10,7 @@ struct CharacterShopView: View {
     var body: some View {
         ZStack {
             //back
-            Image("shopBackgroundImage")
+            Image("shopBackImage")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
